d2c511f6c5c0244c9b2d92b10bd5a56b024f9b48
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-06-02 15:14:18 +0530

    changed 271 files

---
+++ Posts/index.js
@@ -0,0 +1,15 @@
+console.log("Hello Heloo");
+
+const API = "https://jsonplaceholder.typicode.com/posts";
+
+//initial state
+const initialState = {
+  posts: [],
+  loading: false,
+  error: null,
+};
+
+//Create Async Thunk
+
+get
+//Slice
