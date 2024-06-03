0b961a696ee7897f4c4c02125b0c1da543fc05f1
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-06-03 11:49:46 +0530

    modified index.js in Posts

--- Posts/index.js
+++ Posts/index.js
@@ -1,3 +1,6 @@
+const { createAsyncThunk, createSlice } = require("@reduxjs/toolkit");
+const { default: axios } = require("axios");
+
 console.log("Hello Heloo");
 
 const API = "https://jsonplaceholder.typicode.com/posts";
@@ -10,6 +13,20 @@ const initialState = {
 };
 
 //Create Async Thunk
+const fetchPosts = createAsyncThunk("post/fetchPosts", async () => {
+  const data = await axios.get;
+  return data;
+});
 
-get 2
 //Slice
+createSlice({
+  name: "posts",
+  initialState,
+  extraReducers: (builder) => {
+    //handle lifecycle - pending - success - rejected
+    //pending
+  },
+});
+
+//Phaces of Create Async Thunk
+// Pending , FulFilled, rejected
